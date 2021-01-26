b0b3d338a09083cae353fc98ff0c211bdf0fe5c1
antoniukivan <ivan.antonuk1992@gmail.com>
2021-01-26 17:12:43 +0200

    add class MovieServiceImpl with override methods to package service.impl

---
+++ src/main/java/service/impl/MovieServiceImpl.java
@@ -0,0 +1,19 @@
+package service.impl;
+
+import lib.Service;
+import java.util.List;
+import model.Movie;
+import service.MovieService;
+
+@Service
+public class MovieServiceImpl implements MovieService {
+    @Override
+    public Movie add(Movie movie) {
+        return null;
+    }
+
+    @Override
+    public List<Movie> getAll() {
+        return null;
+    }
+}
