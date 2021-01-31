eb895b8250ab6d6e70df214dbf87402fc1657422
antoniukivan <ivan.antonuk1992@gmail.com>
2021-01-31 22:17:07 +0200

    add MovieSessionDao interface to the dao package

---
+++ src/main/java/ua/com/cinema/dao/MovieSessionDao.java
@@ -0,0 +1,11 @@
+package ua.com.cinema.dao;
+
+import java.time.LocalDate;
+import java.util.List;
+import ua.com.cinema.model.MovieSession;
+
+public interface MovieSessionDao {
+    List<MovieSession> findAvailableSessions(Long movieId, LocalDate date);
+
+    MovieSession add(MovieSession movieSession);
+}
