56b1c19f415d40df3ed08bc933d4b69cb2af9ea4
antoniukivan <ivan.antonuk1992@gmail.com>
2021-02-14 22:43:09 +0200

    add UserResponseDto class to the model.dto package

---
+++ src/main/java/ua/com/cinema/model/dto/user/UserResponseDto.java
@@ -0,0 +1,24 @@
+package ua.com.cinema.model.dto.user;
+
+public class UserResponseDto {
+    private Long id;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public UserResponseDto setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public UserResponseDto setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+}
