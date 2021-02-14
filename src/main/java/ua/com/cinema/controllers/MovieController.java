9ee0c75445aa85b7e4eafa983130d7bbca4782f3
antoniukivan <ivan.antonuk1992@gmail.com>
2021-02-14 21:25:32 +0200

    add MovieResponseDto class to the model.dto package

---
+++ src/main/java/ua/com/cinema/controllers/MovieController.java
@@ -0,0 +1,10 @@
+package ua.com.cinema.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/movie")
+public class MovieController {
+
+}
