4d541bd06678f7eba0a45e0f5680f51643d1f7b1
antoniukivan <ivan.antonuk1992@gmail.com>
2021-02-14 23:25:42 +0200

    add CinemaHallRequestDto class to the model.dto package

---
+++ src/main/java/ua/com/cinema/model/dto/CinemaHallRequestDto.java
@@ -0,0 +1,13 @@
+package ua.com.cinema.model.dto;
+
+public class CinemaHallRequestDto {
+    private int capacity;
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}
