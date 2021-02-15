ee8a15e9c1b007ce47bd1e879898b77984438804
antoniukivan <ivan.antonuk1992@gmail.com>
2021-02-15 18:14:03 +0200

    add UserMapper class to the mappers package

---
+++ src/main/java/ua/com/cinema/service/mappers/UserMapper.java
@@ -0,0 +1,23 @@
+package ua.com.cinema.service.mappers;
+
+import ua.com.cinema.model.User;
+import ua.com.cinema.model.dto.UserRequestDto;
+import ua.com.cinema.model.dto.UserResponseDto;
+
+public class UserMapper implements DtoMapper<User, UserRequestDto, UserResponseDto> {
+    @Override
+    public User getModelFromDto(UserRequestDto requestDto) {
+        User user = new User();
+        user.setEmail(requestDto.getEmail());
+        user.setPassword(requestDto.getPassword());
+        return user;
+    }
+
+    @Override
+    public UserResponseDto getDtoFromModel(User user) {
+        UserResponseDto userResponseDto = new UserResponseDto();
+        userResponseDto.setId(user.getId());
+        userResponseDto.setEmail(user.getEmail());
+        return userResponseDto;
+    }
+}
