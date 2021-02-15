01b3b4b2b215f496f86d0cfce1714a5933754f81
antoniukivan <ivan.antonuk1992@gmail.com>
2021-02-15 20:17:43 +0200

    add OrderMapper class to the mappers package

--- src/main/java/ua/com/cinema/dao/TicketDao.java
+++ src/main/java/ua/com/cinema/dao/TicketDao.java
@@ -1,7 +1,11 @@
 package ua.com.cinema.dao;
 
+import java.util.List;
 import ua.com.cinema.model.Ticket;
+import ua.com.cinema.model.User;
 
 public interface TicketDao {
     Ticket add(Ticket ticket);
+
+    List<Ticket> getAllByUser(User user);
 }
--- src/main/java/ua/com/cinema/service/mappers/OrderMapper.java
+++ src/main/java/ua/com/cinema/service/mappers/OrderMapper.java
@@ -1,28 +1,44 @@
 package ua.com.cinema.service.mappers;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 import ua.com.cinema.model.Order;
+import ua.com.cinema.model.Ticket;
 import ua.com.cinema.model.dto.OrderRequestDto;
 import ua.com.cinema.model.dto.OrderResponseDto;
+import ua.com.cinema.service.TicketService;
 import ua.com.cinema.service.UserService;
 
 @Component
 public class OrderMapper implements DtoMapper<Order, OrderRequestDto, OrderResponseDto> {
     private final UserService userService;
-    private final
+    private final TicketService ticketService;
+
+    public OrderMapper(UserService userService, TicketService ticketService) {
+        this.userService = userService;
+        this.ticketService = ticketService;
+    }
 
     @Override
     public Order getModelFromDto(OrderRequestDto requestDto) {
         Order order = new Order();
         order.setUser(userService.findByEmail(requestDto.getEmail()).get());
         order.setPurchaseTime(LocalDateTime.parse(requestDto.getPurchaseTime()));
-        order.set
+        order.setTickets(ticketService.getAllByUser(order.getUser()));
         return order;
     }
 
     @Override
-    public OrderResponseDto getDtoFromModel(Order entity) {
-        return null;
+    public OrderResponseDto getDtoFromModel(Order order) {
+        OrderResponseDto orderResponseDto = new OrderResponseDto();
+        orderResponseDto.setId(order.getId());
+        orderResponseDto.setPurchaseTime(order.getPurchaseTime().toString());
+        List<Long> ticketsId = ticketService.getAllByUser(order.getUser()).stream()
+                .map(Ticket::getId)
+                .collect(Collectors.toList());
+        orderResponseDto.setTickets(ticketsId);
+        return orderResponseDto;
     }
 }
