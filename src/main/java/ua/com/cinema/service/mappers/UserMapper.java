24768a6e65ffd625be7dafb6df7bde2aed1634f3
antoniukivan <ivan.antonuk1992@gmail.com>
2021-02-15 18:19:36 +0200

    add UserController class to the controllers package

--- src/main/java/ua/com/cinema/service/mappers/UserMapper.java
+++ src/main/java/ua/com/cinema/service/mappers/UserMapper.java
@@ -1,9 +1,11 @@
 package ua.com.cinema.service.mappers;
 
+import org.springframework.stereotype.Component;
 import ua.com.cinema.model.User;
 import ua.com.cinema.model.dto.UserRequestDto;
 import ua.com.cinema.model.dto.UserResponseDto;
 
+@Component
 public class UserMapper implements DtoMapper<User, UserRequestDto, UserResponseDto> {
     @Override
     public User getModelFromDto(UserRequestDto requestDto) {
