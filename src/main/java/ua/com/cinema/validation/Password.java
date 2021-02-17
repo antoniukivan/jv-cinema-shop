5ac1fe10f4147b4a56c36a2cd7afedec9b15ad6e
antoniukivan <ivan.antonuk1992@gmail.com>
2021-02-17 21:15:20 +0200

    add PasswordValidator class to the validation package

--- src/main/java/ua/com/cinema/validation/Password.java
+++ src/main/java/ua/com/cinema/validation/Password.java
@@ -1,19 +1,25 @@
 package ua.com.cinema.validation;
 
 import jakarta.validation.Constraint;
-import jakarta.validation.Payload;
-import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Documented
-@Constraint(validatedBy = EmailValidator.class)
-@Target( { ElementType.METHOD, ElementType.FIELD })
+@Constraint(validatedBy = PasswordValidator.class)
+@Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Password {
-    String message() default "Invalid password";
-    Class<?>[] groups() default {};
-    Class<? extends Payload>[] payload() default {};
+
+    String message() default "Passwords don't match!";
+
+    String field();
+
+    String fieldMatch();
+
+    @Target({ElementType.TYPE})
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface List {
+        Password[] value();
+    }
 }
---
+++ src/main/java/ua/com/cinema/validation/PasswordValidator.java
@@ -0,0 +1,29 @@
+package ua.com.cinema.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.beans.BeanWrapperImpl;
+
+public class PasswordValidator implements ConstraintValidator<Password, String> {
+    private String field;
+    private String fieldMatch;
+
+    public void initialize(Password constraintAnnotation) {
+        this.field = constraintAnnotation.field();
+        this.fieldMatch = constraintAnnotation.fieldMatch();
+    }
+
+    @Override
+    public boolean isValid(String password, ConstraintValidatorContext constraintValidatorContext) {
+        Object fieldValue = new BeanWrapperImpl(password)
+                .getPropertyValue(field);
+        Object fieldMatchValue = new BeanWrapperImpl(password)
+                .getPropertyValue(fieldMatch);
+
+        if (fieldValue != null) {
+            return fieldValue.equals(fieldMatchValue);
+        } else {
+            return fieldMatchValue == null;
+        }
+    }
+}
