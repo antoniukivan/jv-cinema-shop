93a0d810a65b035e6890abc9418ebda348d9c477
antoniukivan <ivan.antonuk1992@gmail.com>
2021-02-21 19:41:47 +0200

    add RoleName enum to the model package

--- src/main/java/ua/com/cinema/model/Role.java
+++ src/main/java/ua/com/cinema/model/Role.java
@@ -1,5 +1,30 @@
 package ua.com.cinema.model;
 
-public enum Role {
-    ADMIN, USER
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private RoleName roleName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public RoleName getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(RoleName roleName) {
+        this.roleName = roleName;
+    }
 }
